e4dcdedb0597b0d2f0cf07c52dc7c484782b048d
Hegde07 <hegdebhargav92@gmail.com>
2023-09-04 23:47:51 -0700

    added checkAnswer function

--- src/App.js
+++ src/App.js
@@ -1,40 +1,48 @@
-import nations from './nations'
+import nations from "./nations";
 import "/node_modules/flag-icons/css/flag-icons.css";
-import './App.css';
-import { useEffect, useState } from 'react';
+import "./App.css";
+import { useEffect, useState } from "react";
 function App() {
-  const [country,setCountry]=useState([]);
-  const [flagCountry,setFlagCountry]=useState({})
-  const generateRandomNations =()=>{
-      let ct = []
-      for(let i=0;i<4;i++){
-     const r = Math.floor( Math.random()*nations.length)//generating random index
-     ct.push(nations[r]);//This is the index of nation
-      }
-      
-      setCountry(ct)
-      const index = Math.floor(Math.random()*4)
-      setFlagCountry(ct[index])
-      console.log(ct,ct[index])
+  const [country, setCountry] = useState([]);
+  const [flagCountry, setFlagCountry] = useState({});
+  const generateRandomNations = () => {
+    let ct = [];
+    for (let i = 0; i < 4; i++) {
+      const r = Math.floor(Math.random() * nations.length); //generating random index
+      ct.push(nations[r]); //This is the index of nation
+    }
+
+    setCountry(ct);
+    const index = Math.floor(Math.random() * 4);
+    setFlagCountry(ct[index]);
+  };
+
+  const nextQuestion =()=>{
+    generateRandomNations()
   }
 
-  useEffect(()=>{
-   generateRandomNations();
-  },[])
-  const checkAnswer=(country)=>{
-    if(country.name === flagCountry.name){
-     alert('correct')
-    }else{
-      alert('incorrect')
+  useEffect(() => {
+    generateRandomNations();
+  }, []);
+  const checkAnswer = (country) => {
+    if (country.name === flagCountry.name) {
+      alert("correct");
+    } else {
+      alert("incorrect");
     }
- }
+    nextQuestion();
+  };
   return (
     <div className="App">
-      <span className="fi fi-gr fis"></span>
-      <div className='buttons'>
-         {country.map(c=>(
-           <button onClick={e=>checkAnswer(c)}>{c.name}</button>
-         ))}
+      {flagCountry.name ? (
+        <span
+          className={`fi fi-${flagCountry["alpha-2"].toLowerCase()} fis`}
+        ></span>
+      ) : null}
+      <div className="buttons">
+        {country.map((c) => (
+          <button onClick={(e) => checkAnswer(c)}>{c.name}</button>
+        ))}
       </div>
     </div>
   );
